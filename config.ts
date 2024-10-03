2b441043ccff466cc4f4b51bc7ab55589ea03710
lucianoojeda36 <lucianoojeda36@gmail.com>
2024-10-03 18:18:14 -0300

    update

--- config.ts
+++ config.ts
@@ -1,8 +1,7 @@
 import dotenv from 'dotenv';
 import path from 'path';
 
-dotenv.config({ path: path.resolve(__dirname, '../.env') });
-
+dotenv.config({ path: path.resolve(__dirname, './.env') });
 interface Config {
   authServiceUrl: string;
   service1Url: string;
@@ -11,11 +10,8 @@ interface Config {
 
 const config: Config = {
   authServiceUrl: process.env.AUTH_SERVICE_URL || 'http://localhost:3001',
-  service1Url: process.env.SERVICE1_URL || 'http://localhost:3002',
-  jwtSecretKey:
-    process.env.JWT_SECRET_KEY ||
-    process.env.SECRET_KEY ||
-    'default_secret_key',
+  service1Url: process.env.SERVICE_1_URL || 'http://localhost:3002',
+  jwtSecretKey: process.env.JWT_SECRET_KEY || 'default_key',
 };
 
 export default config;
